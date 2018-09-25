049c44f683803a6e5278885683fa3db78067c31f
Charley McGowan and Quinn Miller <charley+quinn@>
2018-09-24 19:05:23 -0700

    made the fav things projects using arrays

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  var ArrayOutput = [];
+  $("form").submit(function (event) {
+  var foodinput = $("#fav1").val();
+  var carinput = $("#fav2").val();
+  var placeinput = $("#fav3").val();
+  event.preventDefault();
+  function getfavs() {
+    ArrayOutput.push(foodinput);
+    ArrayOutput.push(carinput);
+    ArrayOutput.push(placeinput);
+    console.log(ArrayOutput);
+    };
+    getfavs();
+    $("span#fav1answer").text(ArrayOutput[0]);
+    $("span#fav2answer").text(ArrayOutput[1]);
+    $("span#fav3answer").text(ArrayOutput[2]);
+  });
+});
